89bfcb39ad91291905a77b94ac405d08ef55e211
ANULAL P <anulalsn@gmail.com>
2025-12-31 14:46:13 +0530

    EmployeeHub Pro - vercel setup commit

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
   })
 );
+
 app.use(express.json());
 
 // routes
